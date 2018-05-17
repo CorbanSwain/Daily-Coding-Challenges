984557dfce721e2603876149d9fa39caea3b1b50
CorbanSwain <CorbanSwain@gmail.com>
2018-05-16 20:25:01 -0400

    adding extra checks, modularizing code

--- 1805_may.py
+++ 1805_may.py
@@ -3,17 +3,23 @@
 # Corban Swain, 2018
 # May Daily Coding Challenges
 
-# 180516
-# Given a list of numbers, return whether any two sums to k. 
-# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17. 
-# Bonus: Can you do this in one pass?
+
+def _180516():
+    """Given a list of numbers, return whether any two sums to k.
+    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is
+    17. Bonus: Can you do this in one pass?"""
+
+    def sum_in(ls, k):
+        try:
+            del ls[ls.index(k / 2)]
+        except ValueError:
+            pass
+        return any(k - x in ls for x in ls)
+
+    assert sum_in([10, 50, 3, 7], 17) is True
+    assert sum_in([5, 2, 3], 10) is False
+    assert sum_in([5, 2, 3, 5], 10) is True
 
 
-def sum_in(ls, k):
-    half = k / 2
-    if half in ls:
-        del ls[ls.index(half)]
-    return any(k - x in ls for x in ls)
-
-
-assert sum_in([10, 50, 3, 7], 17)
+if __name__ == "__main__":
+    _180516()
