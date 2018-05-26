4db1fe58dac3efc4031ba515c50599c806489f9a
Corban Swain <CorbanSwain@gmail.com>
2018-05-26 16:36:57 -0400

    may 26 problem only

--- 1805_may_2018.py
+++ 1805_may_2018.py
@@ -564,5 +564,20 @@ def _180525():
     print('Done Main!')
 
 
+def _180526():
+    """This problem was asked by Twitter.
+
+    Implement an autocomplete system. That is, given a query string s and a set
+    of all possible query strings, return all strings in the set that have s as
+    a prefix.
+
+    For example, given the query string de and the set of strings
+    [dog, deer, deal], return [deer, deal].
+
+    Hint: Try preprocessing the dictionary into a more efficient data structure
+    to speed up queries."""
+    pass
+
+
 if __name__ == "__main__":
-    _180525()
+    _180526()
