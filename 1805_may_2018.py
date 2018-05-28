18e345939e0f80019f8118e0fb5cee1b05984a68
CorbanSwain <CorbanSwain@gmail.com>
2018-05-27 22:40:03 -0400

    may 27 challenge, update

--- 1805_may_2018.py
+++ 1805_may_2018.py
@@ -700,5 +700,68 @@ def _180526():
     # especially for a large dictionary.
 
 
+def _180527():
+    """This problem was asked by Amazon.
+
+    There exists a staircase with N steps, and you can climb up either 1 or 2
+    steps at a time. Given N, write a function that returns the number of unique
+    ways you can climb the staircase. The order of the steps matters.
+
+    For example, if N is 4, then there are 5 unique ways:
+
+        1, 1, 1, 1
+        2, 1, 1
+        1, 2, 1
+        1, 1, 2
+        2, 2
+
+    What if, instead of being able to climb 1 or 2 steps at a time, you could
+    climb any number from a set of positive integers X? For example, if
+    X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time."""
+
+    from functools import reduce
+
+    # if n = num_steps return 1, if n < num_steps return 0 else return
+    # ways to climb(n - num_steps)
+    def ways_to_climb(n, num_steps=None):
+        if num_steps is None:
+            return ways_to_climb(n, 1) + ways_to_climb(n, 2)
+        n = n - num_steps
+        if n < 0:
+            return 0
+        elif n == 0:
+            return 1
+        else:
+            return ways_to_climb(n)
+
+    # for the advanced case, simply go through the set of possible steps in
+    # the initial setup
+    def ways_to_climb_2(n, step_set, num_steps=None):
+        if num_steps is None:
+            return reduce(
+                lambda tot, x: tot + ways_to_climb_2(n, step_set, x),
+                step_set, 0)
+
+        n = n - num_steps
+        if n < 0:
+            return 0
+        elif n == 0:
+            return 1
+        else:
+            return ways_to_climb_2(n, step_set)
+
+    # TESTS
+    assert ways_to_climb(4) == 5
+    assert ways_to_climb_2(5, {1, 3, 5}) == 5
+    '''
+    Ways to climb 5 steps taking 1, 3, or 5 steps at a time:
+    1: 1, 1, 1, 1, 1
+    2: 1, 3, 1
+    3: 1, 1, 3
+    4: 3, 1, 1
+    5: 5
+    '''
+
+
 if __name__ == "__main__":
-    _180526()
+    _180527()
